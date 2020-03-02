a6db020b012cbbf96f5e7bdc3019abb8c5652fcf
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-03-01 21:52:50 -0500

    Added icon for weather status

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,23 +1,75 @@
 
 // Use OpenWeather API to retrive weather data for cities
 var APIkey = "f1aa41731ebff07c25c28f270a17faac";
-// Bujumbura ID
+// Bujumbura ID for testing
 var cityID = "425378";
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&APPID=" + APIkey;
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&appid=" + APIkey;
 
 
 
-// Use AJAX to hook into API & retrieve data in JSON format
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
+// var AJAXbyCity = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&appid=" + APIkey;
 
-.then(function(response) {
-    // Returns city name
-    // console.log(response.city.name);
 
-});
+$( "#cityEntered" ).keypress(function(e) {
+    if(e.which == 13) {
+        var cityEntry = $("#cityEntered").val(); 
+        //console.log("City Entered: " + cityEntry);
+
+        // Use AJAX to hook into API & retrieve data in JSON format
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&units=imperial&appid=" + APIkey;
+        // api.openweathermap.org/data/2.5/weather?q=London&units=imperial
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            // Returns city name
+            console.log(response);
+            console.log(response.weather[0].main);
+            // City name to HTML
+            $("#htmlCity").text(response.name);
+            $("#htmlTemp").text(response.main.temp + "° F");
+            $("#htmlHumid").text(response.main.humidity + " RH");
+            $("#htmlWind").text(response.wind.speed + " MPH");
+            $("#htmlCondition").text(response.weather[0].main);           
+
+
+            // Image based on weather conditions
+            var dataIcon = $("#htmlIcon");
+            if (response.weather[0].main == "Rain") {
+                dataIcon.attr("data-icon", "wi:day-rain");
+            } else if (response.weather[0].main == "Thunderstorm") {
+                dataIcon.attr("data-icon", "wi:day-storm-showers");
+            } else if (response.weather[0].main == "Drizzle") {
+                dataIcon.attr("data-icon", "wi:day-rain-mix");
+            } else if (response.weather[0].main == "Snow") {
+                dataIcon.attr("data-icon", "wi:day-snow");
+            } else if (response.weather[0].main == "Clouds") {
+                dataIcon.attr("data-icon", "wi:day-cloudy");
+            } else {
+                dataIcon.attr("data-icon", "wi:day-sunny");
+            }
+
+        });
+
+
+
+        // Today's date to HTML
+        var todaysDate = new Date();
+        var year = todaysDate.getFullYear();
+        var month = todaysDate.getMonth() + 1;
+        var day = todaysDate.getDate();
+        // console.log(month, day, year);
+        $("#htmlDate").text(month + " / " + day + " / " + year);
+    }
+  });
+
+
+
+
+
+
 
 // Display the following:
     // City
